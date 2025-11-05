a4bc71b6259531a640cdbd2ac4b1de44a7bd2b7c
Dominick12 <dominick1710@gmail.com>
2025-11-05 20:43:54 +0100

    ✨ feat(core): add ConfigManager for configuration handling
    Implements a ConfigManager to manage configuration values and cache
    clearing during plugin lifecycle events.

--- src/main/java/it/dominick/th/TreasureHunt.java
+++ src/main/java/it/dominick/th/TreasureHunt.java
@@ -1,17 +1,29 @@
 package it.dominick.th;
 
+import it.dominick.th.config.ConfigManager;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TreasureHunt extends JavaPlugin {
+    @Getter
+    private static volatile TreasureHunt instance;
+    @Getter
+    private ConfigManager configManager;
+
+    @Override
+    public void onLoad() {
+        instance = this;
+    }
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        this.configManager = new ConfigManager(this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        if (configManager != null) {
+            configManager.clearValueCache();
+        }
     }
 }
